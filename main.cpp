4d45f7e65f6559155e7cfc22ac7a599e85e9d577
Loren Burkholder <computersemiexpert@outlook.com>
2024-03-02 14:19:51 -0500

    Add a dialog to monitor CPU usage

--- main.cpp
+++ main.cpp
@@ -47,6 +47,67 @@ private:
     int m_uptimeTimer;
 };
 
+class CpuDialog : public finalcut::FDialog
+{
+public:
+    CpuDialog(finalcut::FWidget *parent)
+        : finalcut::FDialog(parent)
+    {
+        setText("CPU");
+        setGeometry({4, 9}, {60, 7});
+
+        m_cpuMeter = new finalcut::FProgressbar{this};
+        m_cpuMeter->setGeometry({3, 2}, {50, 1});
+        parseCpuUsage();
+
+        m_cpuInfo = new finalcut::FLabel{this};
+        m_cpuInfo->setGeometry({3, 4}, {60, 1});
+
+        std::ifstream cpuinfo("/proc/cpuinfo");
+        std::string line;
+        while (std::getline(cpuinfo, line))
+            if (line.starts_with("model name"))
+                break;
+        m_cpuInfo->setText(line.substr(line.find(':') + 2));
+
+        m_cpuUsageTimer = addTimer(500);
+    }
+
+    void parseCpuUsage()
+    {
+        std::ifstream proc_stat("/proc/stat");
+        std::string cpuLine;
+        std::getline(proc_stat, cpuLine);
+        std::string cpu;
+        int user, nice, system, idle, iowait, irq, softirq, steal, guest, guestnice;
+        std::stringstream cpuStream(cpuLine);
+        cpuStream >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal >> guest >> guestnice;
+        double totalTime = user + nice + system + idle + iowait + irq + softirq + steal + guest + guestnice;
+        double activeTime = user + nice + system + irq + softirq + steal + guest + guestnice;
+        m_cpuMeter->reset();
+        m_cpuMeter->setPercentage(((activeTime - m_activeCpuLastTick) / (totalTime - m_totalCpuLastTick)) * 100);
+        m_totalCpuLastTick = totalTime;
+        m_activeCpuLastTick = activeTime;
+    }
+
+    void onTimer(finalcut::FTimerEvent *e) override
+    {
+        if (e->getTimerId() == m_cpuUsageTimer)
+        {
+            parseCpuUsage();
+            m_cpuMeter->redraw();
+        }
+    }
+
+private:
+    finalcut::FProgressbar *m_cpuMeter;
+    finalcut::FLabel *m_cpuInfo;
+    int m_cpuUsageTimer;
+
+    double m_totalCpuLastTick = -1;
+    double m_activeCpuLastTick = -1;
+};
+
 int main(int argc, char *argv[])
 {
     finalcut::FApplication app(argc, argv);
@@ -54,6 +115,9 @@ int main(int argc, char *argv[])
     MainDialog mainDialog(&app);
     mainDialog.show();
     finalcut::FWidget::setMainWidget(&mainDialog);
+
+    CpuDialog cpuDialog(&mainDialog);
+    cpuDialog.show();
 
     return app.exec();
 }
